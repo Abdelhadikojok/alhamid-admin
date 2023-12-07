de813fb1b80252e5d3e3daaf5aa20d5eb81a6366
Abdelhadikojok <119413261+Abdelhadikojok@users.noreply.github.com>
2023-12-07 18:09:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Items from "./components/dashboard/Items";
+import AddItem from "./components/add-item/AddItem";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/add-item" element={<AddItem />} />
+        <Route path="/" element={<Items />} />
+        <Route path="/add-item/:id" element={<AddItem />} />
+      </Routes>
+    </Router>
   );
 }
 
